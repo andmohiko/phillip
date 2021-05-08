50b83c352aa8e0f99531e365a6f1b512a21b9568
andmohiko <andmohiko@gmail.com>
2021-05-08 21:28:54 +0900

    add base components

---
+++ src/components/input/TextField.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="flex flex-col mb-4 items-start">
+    <label
+      v-show="isLabelShow"
+      class="text-gray-700 text-left"
+      >{{ label }}
+    </label>
+    <div
+      class="pt-2 flex items-center"
+      :style="[widthStyle]"
+    >
+      <span v-if="prefix" class="text-lg text-gray-700 pr-1">{{ prefix }}</span>
+      <input
+        v-model="input"
+        :placeholder="placeholder"
+        class="border py-2 px-3 text-grey-darkest w-full"
+        type="text"
+      >
+      <span v-if="suffix" class="text-lg text-gray-700 pl-1">{{ suffix }}</span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    label: {
+      required: true,
+      type: String
+    },
+    placeholder: {
+      required: true,
+      type: String
+    },
+    isLabelShow: {
+      default: true,
+      type: Boolean
+    },
+    allowEmpty: {
+      default: true,
+      type: Boolean
+    },
+    maxLength: {
+      required: false,
+      type: Number
+    },
+    defaultValue: {
+      default: '',
+      type: String
+    },
+    width: {
+      default: '100%',
+      type: String
+    },
+    prefix: {
+      required: false,
+      type: String
+    },
+    suffix: {
+      required: false,
+      type: String
+    }
+  },
+  data() {
+    return {
+      input: this.defaultValue,
+      widthStyle: {
+        width: this.width
+      }
+    }
+  },
+  computed: {
+    isOverLength(): boolean {
+      return !!(this.maxLength && this.input.length > this.maxLength)
+    },
+    isValid(): boolean {
+      if (!this.allowEmpty && this.input === '') return false
+      if (this.isOverLength) return false
+      return true
+    }
+  },
+  methods: {
+    get(): string | null {
+      if (!this.isValid) return null
+      return this.input
+    }
+  }
+})
+</script>
+
+<style scoped lang="scss">
+</style>
---
+++ src/components/parts/Button.vue
@@ -0,0 +1,45 @@
+<template>
+  <button
+    :style="widthStyle"
+    class="uppercase rounded-full max-w-max shadow-sm hover:shadow-md button"
+    @click="$emit('onClick')"
+  >
+    {{ label }}
+  </button>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    label: {
+      default: 'ボタン',
+      type: String
+    },
+    width: {
+      default: '240px',
+      type: String
+    }
+  },
+  data() {
+    return {
+      widthStyle: {
+        width: this.width
+      }
+    }
+  }
+})
+</script>
+
+<style lang="scss">
+.button {
+  margin: 0 auto;
+  display: block;
+  border: solid 1px #69a5ff;
+  color: #69a5ff;
+  padding: 15px 0;
+  font-weight: bold;
+  font-size: 14px;
+  letter-spacing: 1px;
+}
+</style>
