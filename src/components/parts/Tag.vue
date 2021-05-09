f707705725dd39329288789e8cca215049b05995
andmohiko <andmohiko@gmail.com>
2021-05-10 03:01:21 +0900

    add TagsSelecter

---
+++ src/components/parts/Tag.vue
@@ -0,0 +1,44 @@
+<template>
+  <button
+    @click="$emit('onSelect')"
+    class="tag"
+    :class="{ tagSelected: isSelected }"
+  >
+    {{ label }}
+  </button>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    label: {
+      required: true,
+      type: String
+    },
+    isSelected: {
+      default: false,
+      type: Boolean
+    }
+  }
+})
+</script>
+
+<style scoped lang="scss">
+.tag {
+  display: inline-block;
+  border: 2px solid #AAAAFF;
+  padding: 4px 12px;
+  margin: 4px;
+  border-radius: 50px;
+  font-size: 16px;
+  font-weight: bold;
+  color: #888888;
+  text-align: center;
+}
+.tagSelected {
+  color: #FFFFFF;
+  background: #AAAAFF;
+}
+</style>
--- src/components/parts/TagsSelecter.vue
+++ src/components/parts/TagsSelecter.vue
@@ -1,21 +1,73 @@
 <template>
-  <div class="add-bookmark pb-6 border border-blue-400">
-    <p class="text-l pb-2">タグ一覧</p>
-    {{ tags }}
+  <div class="tags-selecter pb-6">
+    <p class="text-gray-700 text-left">タグ一覧</p>
+    <div>
+      <Tag
+        v-for="(tag, i) in tags"
+        :key="tag.id"
+        :label="tag"
+        :isSelected="isSelectedArray[i]"
+        @onSelect="selectTag(i)"
+      />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import { Label } from '@/types/Tag'
+import Tag from '@/components/parts/Tag.vue'
+
+export type LocalState = {
+  isSelectedArray: boolean[]
+}
 
 export default Vue.extend({
+  components: {
+    Tag
+  },
+  data(): LocalState {
+    return {
+      isSelectedArray: []
+    }
+  },
+  mounted() {
+    this.isSelectedArray = new Array(this.tags.length).fill(false);
+  },
   computed: {
     tags() {
       return this.$store.state.tags
+    }
+  },
+  methods: {
+    selectTag(idx: number) {
+      this.isSelectedArray = this.isSelectedArray.map((flag, i) => {
+        if (i === idx) {
+          return !flag
+        }
+        return flag
+      })
+    },
+    get(): Label[] {
+      return this.tags.filter((tag, i: number) => this.isSelectedArray[i])
     }
   }
 })
 </script>
 
 <style scoped lang="scss">
+.tag {
+  border: 2px solid #AAAAFF;
+  padding: 4px 12px;
+  margin: 4px;
+  border-radius: 50px;
+  font-size: 16px;
+  font-weight: bold;
+  color: #888888;
+  text-align: center;
+}
+.tagSelected {
+  color: #FFFFFF;
+  background: #AAAAFF;
+}
 </style>
